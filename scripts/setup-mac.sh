2803957d27ec1477a533f0eba1a43f5b08827ba0
mytram <mytram2@gmail.com>
2019-11-11 10:21:44 +1100

    Add some slides

---
+++ scripts/setup-mac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+brew install minikube
+brew install hyperkit
+
+minikube config set vm-driver hyperkit
+minikube addons enable heapster
+minikube addons enable ingress
+
+# minikube start --vm-driver=hyperkit
